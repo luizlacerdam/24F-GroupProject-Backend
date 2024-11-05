cd5cc7651bff02fc11f2cb5d65caf25ff6c045d9
Ricardo199 <ricardo1529@gmail.com>
2024-11-05 18:42:13 -0500

    Refactor database connection string to use environment variable for password

--- src/config/config.js
+++ src/config/config.js
@@ -3,9 +3,9 @@ import dotenv from 'dotenv';
 dotenv.config({ path: './config/secrets.env' });
 
 const password = process.env.DB_PASSWORD;
-const connectionString = 'mongodb+srv://gustavo213098:' + 
-    `${password}@c1.3o2bb.mongodb.net/groupProject?retryWrites=true&` +
-    'w=majority&appName=c1';
+const connectionString = 'mongodb+srv://gustavo213098:' 
+    + `${password}@c1.3o2bb.mongodb.net/groupProject?retryWrites=true&` 
+    + 'w=majority&appName=c1';
 
 export default {
     atlasDb: connectionString,
