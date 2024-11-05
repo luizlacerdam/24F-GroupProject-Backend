954e0c3b3a1e0be096d6ac65b21db7dc0b94dda0
Ricardo199 <ricardo1529@gmail.com>
2024-11-05 18:59:13 -0500

    Add JSDoc comments for database connection function

--- src/database/mongoose.js
+++ src/database/mongoose.js
@@ -2,6 +2,17 @@ import mongoose from 'mongoose';
 import config from '../config/config';
 
 // eslint-disable-next-line import/prefer-default-export
+/**
+ * Connects to the MongoDB database using Mongoose.
+ * 
+ * This function establishes a connection to the MongoDB database specified in the configuration.
+ * It sets up event listeners to handle connection errors and logs a message when the connection is successfully opened.
+ * 
+ * @function
+ * @name connectToDatabase
+ */
+
+// eslint-disable-next-line import/prefer-default-export
 export const connectToDatabase = function connectToDatabase() {
     mongoose.connect(config.atlasDb);
 
