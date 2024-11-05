c73759504f32fecd02fec785bd426b69e48a70e0
Ricardo199 <ricardo1529@gmail.com>
2024-11-05 18:41:26 -0500

    Refactor database connection to use environment variables and remove hardcoded credentials; add license information to package-lock.json; recreate Express router in a new file.

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,12 @@
-module.exports = {
-    ATLASDB: 'mongodb+srv://gustavo213098:tavitoperro@c1.3o2bb.mongodb.net/groupProject?retryWrites=true&w=majority&appName=c1'
-}
+import dotenv from 'dotenv';
+
+dotenv.config({ path: './config/secrets.env' });
+
+const password = process.env.DB_PASSWORD;
+const connectionString = 'mongodb+srv://gustavo213098:' + 
+    `${password}@c1.3o2bb.mongodb.net/groupProject?retryWrites=true&` +
+    'w=majority&appName=c1';
+
+export default {
+    atlasDb: connectionString,
+};
