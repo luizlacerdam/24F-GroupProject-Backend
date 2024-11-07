c62470f9c7e170be02d3ff589de7c7fa7cb68e74
Ricardo199 <ricardo1529@gmail.com>
2024-11-06 21:53:29 -0500

    Add user routes and update MongoDB connection string with environment variable

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,11 @@
+// Load environment variables from the secrets file
+require('dotenv').config({ path: './config/secrets.env' });
+
+// Construct the MongoDB connection string using environment variable for the password
+// eslint-disable-next-line max-len
+const connectionString = `mongodb+srv://ricardo1529:${process.env.DB_PASSWORD}@midterm.fuok2.mongodb.net/?retryWrites=true&w=majority&appName=Midterm`;
+
+// Export the connection string for use in the application
 module.exports = {
-    ATLASDB: 'mongodb+srv://gustavo213098:tavitoperro@c1.3o2bb.mongodb.net/groupProject?retryWrites=true&w=majority&appName=c1'
-}
+    ATLASDB: connectionString,
+};
